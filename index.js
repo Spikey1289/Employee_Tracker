28dcc0f5c6955a0ae473f0389c9fc863940b9ed2
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-02-16 14:45:06 -0500

    added dotenv support, added a .env for the user to add their credentials to

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const mysql = require('msql2');
+const mysql = require('msql2/promise');
 const inquirer = require('inquirer');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
+const db = /*await*/ mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
