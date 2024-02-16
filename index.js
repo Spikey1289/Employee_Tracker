ae91b4ddf50e0f3fcd45b0f5bb4a45820de7fa35
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-02-16 13:47:58 -0500

    changed server.js to index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mysql = require('msql2');
 const inquirer = require('inquirer');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
+const app = express();
+
